00793cc373b5d4b60ad3f923d8da096a4900a56a
yerimgim <imyrgim92@gmail.com>
2026-09-13 22:08:33 +0900

    feat: shadcn drawer ui

--- src/components/RecordDrawer.tsx
+++ src/components/RecordDrawer.tsx
@@ -1,4 +1,4 @@
-import { Sparkles, X } from "lucide-react";
+import { X } from "lucide-react";
 import {
   Drawer,
   DrawerClose,
@@ -13,7 +13,6 @@ export const RecordDrawer = ({ isOpen, onClose, onSubmit }) => {
       <DrawerContent className="w-full max-w-107.5 h-dvh mx-auto flex items-center justify-between shrink-0 bg-white">
         <DrawerHeader className="border-b border-neutral-100 px-5 py-3.5 flex items-center justify-between shrink-0">
           <DrawerTitle className="font-black text-neutral-900 flex items-center gap-1.5">
-            {/* <Sparkles className="w-4 h-4 text-amber-500" /> */}
             방탈출 기록하기
           </DrawerTitle>
           <DrawerClose className="p-1.5 text-neutral-400 hover:text-neutral-700 rounded-full transition-colors">
